829f61100903222b1ba38349569aca04e4e8964b
Alejandro Sanchez <alejandro@ilogica.cl>
2020-03-31 22:47:01 -0500

    Starter project

---
+++ TravelExpenses.Prism/TravelExpenses.Prism/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using TravelExpenses.Prism.Services;
+using TravelExpenses.Prism.Views;
+
+namespace TravelExpenses.Prism
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
